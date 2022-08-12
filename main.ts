41c8f68ec270120e4627771524a828928b577173
宇佐美隆一 <80155159+chateight@users.noreply.github.com>
2022-08-12 20:31:24 +0900

    # exchange the numbers between multi units

--- main.ts
+++ main.ts
@@ -1,10 +1,25 @@
-music.playMelody("D E G A B B A A ", 250)
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showString("" + (receivedNumber))
 })
-control.inBackground(function () {
+basic.showLeds(`
+    . # . # .
+    # . # . #
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
+music.playMelody("D E G A B B A A ", 250)
+radio.setGroup(1)
+basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+        radio.setGroup(1)
+        radio.sendNumber(1)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        music.playTone(330, music.beat(BeatFraction.Whole))
         radio.setGroup(1)
         radio.sendNumber(0)
     }
+    basic.pause(200)
 })
